158451362442a99e31129daa1c2bbaade8c7776c
Lionel Arucy <lionarucbiz@gmail.com>
2020-05-22 15:06:46 -0400

    update ns cli version

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ import routes from "./routes";
 const appSettings = require("tns-core-modules/application-settings");
 
 const tok = appSettings.getString("tok", null);
-
 // Uncommment the following to see NativeScript-Vue output logs
 Vue.config.silent = false;
 
